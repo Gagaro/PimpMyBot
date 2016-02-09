0cb388bdabb3a172ccb3a0ff5d6ab1fcb5690dc9
Gagaro <gagaro42@gmail.com>
2016-02-09 17:07:30 +0100

    feat: bottle

--- pimpmybot/wsgi/main.py
+++ pimpmybot/wsgi/main.py
@@ -1,2 +1,21 @@
-def run(pipe):
-    return
+import os
+
+import bottle
+from bottle import route, run as bottle_run, template
+
+BASE_DIR = os.path.dirname(__file__)
+
+bottle.TEMPLATE_PATH = [os.path.join(BASE_DIR, './templates/')]
+
+
+@route('/')
+def index():
+    return template('index')
+
+
+def run(pipe=None):
+    bottle_run(host='localhost', port=8000)
+
+
+if __name__ == '__main__':
+    run()
---
+++ pimpmybot/wsgi/templates/index.tpl
@@ -0,0 +1,1 @@
+<h1>Pimp My Bot</h1>
