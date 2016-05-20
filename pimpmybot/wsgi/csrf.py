19e68d27d5e85d27fff473a16e92a2f2a056102f
Gagaro <gagaro42@gmail.com>
2016-05-20 14:09:21 +0200

    fix: do not generate csrf token on each request to avoid false csrf error

--- pimpmybot/wsgi/csrf.py
+++ pimpmybot/wsgi/csrf.py
@@ -3,14 +3,34 @@ from __future__ import absolute_import, unicode_literals
 import hashlib
 import os
 
+import six
 from bottle import request, abort, response
 
 
 ROOT = '/'
 CSRF_TOKEN = '_csrf_token'
-EXPIRES = 600  # seconds
+EXPIRES = 1800  # seconds
 ENCODING = 'utf8'
 SECRET = None
+
+
+def get_secret():
+    """ Get secret """
+    global SECRET
+
+    if SECRET is None:
+        # Avoid circular import
+        from utils.config import Configuration
+        SECRET = Configuration.get().secret
+    return SECRET
+
+
+def get_csrf_token():
+    """ Get the csrf_token and return it. """
+    token_csrf = request.get_cookie(CSRF_TOKEN, secret=get_secret(), default=b'')
+    if isinstance(token_csrf, six.binary_type):
+        token_csrf = token_csrf.decode(ENCODING)
+    return token_csrf
 
 
 def generate_csrf_token():
@@ -25,33 +45,29 @@ def generate_csrf_token():
 
     From https://github.com/Outernet-Project/bottle-utils/blob/master/bottle_utils/csrf.py
     """
-    global SECRET
-
-    if SECRET is None:
-        # Avoid circular import
-        from utils.config import Configuration
-        SECRET = Configuration.get().secret
-
     sha256 = hashlib.sha256()
     sha256.update(os.urandom(8))
     token = sha256.hexdigest().encode(ENCODING)
     response.set_cookie(CSRF_TOKEN, token, path=ROOT,
-                        secret=SECRET, max_age=EXPIRES)
-    request.csrf_token = token.decode(ENCODING)
+                        secret=get_secret(), max_age=EXPIRES)
 
 
 def require_csrf(callback):
     """ Bottle module to automatically check csrf protection """
     def wrapper(*args, **kwargs):
         if request.method == 'POST':
-            token_csrf = request.get_cookie(CSRF_TOKEN, secret=SECRET, default=b'').decode('utf8')
+            token_csrf = get_csrf_token()
             csrf = request.headers.get('X-CsrfToken', None)
             if csrf is None:
                 csrf = request.forms.get(CSRF_TOKEN, None)
             if csrf is None or csrf != token_csrf:
+                # Regenerate token to avoid deadlocking in case of bad formatted cookie
+                generate_csrf_token()
                 abort(400, 'CSRF')
-        if not request.is_ajax:
+        elif not request.get_cookie(CSRF_TOKEN, secret=SECRET, default=b''):
             generate_csrf_token()
+        # set token on request to have it in the template
+        request.csrf_token = get_csrf_token()
         body = callback(*args, **kwargs)
         return body
 
